69a30fb9d071ed1b1674ef8cb544d0d201fb5ae0
David Baker <dave@matrix.org>
2020-02-18 11:05:32 +0000

    also check for patch

--- hak/matrix-seshat/check.js
+++ hak/matrix-seshat/check.js
@@ -37,6 +37,7 @@ module.exports = async function(hakEnv, moduleInfo) {
     const tools = [];
     if (hakEnv.isWin()) {
         tools.push(['perl', '--version']); // for openssl configure
+        tools.push(['patch', '--version']); // to patch sqlcipher Makefile.msc
         tools.push(['nmake', '/?']);
     } else {
         tools.push(['make', '--version']);
