bbc44801c2198ace0409b50ac6035c5f4781416d
Michael Telatynski <7t3chguy@gmail.com>
2020-03-02 15:49:37 +0000

    add missing protocol.js

---
+++ src/protocol.js
@@ -0,0 +1,51 @@
+/*
+Copyright 2020 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+const {app} = require('electron');
+
+const processUrl = (url) => {
+    if (!global.mainWindow) return;
+    console.log("Handling link: ", url);
+    global.mainWindow.loadURL(url.replace("riot://", "vector://"));
+};
+
+module.exports = () => {
+    // get all args except `hidden` as it'd mean the app would not get focused
+    const args = process.argv.slice(1).filter(arg => arg !== "--hidden" && arg !== "-hidden");
+    if (app.isPackaged) {
+        app.setAsDefaultProtocolClient('riot', process.execPath, args);
+    } else {
+        // special handler for running without being packaged, e.g `electron .` by passing our app path to electron
+        app.setAsDefaultProtocolClient('riot', process.execPath, [app.getAppPath(), ...args]);
+    }
+
+    // Protocol handler for macos
+    app.on('open-url', function(ev, url) {
+        ev.preventDefault();
+        processUrl(url);
+    });
+
+    // Protocol handler for win32/Linux
+    if (process.platform !== 'darwin') {
+        app.on('second-instance', (ev, commandLine) => {
+            const url = commandLine[commandLine.length - 1];
+            if (!url.startsWith("riot://")) return;
+            processUrl(url);
+        });
+    }
+};
+
+
