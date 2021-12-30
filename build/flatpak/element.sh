5c6e13dff8412829059aa3f26895c74dd890ee0f
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2021-12-30 11:40:35 +0100

    Fix path to element-desktop executable in the wrapper script

--- build/flatpak/element.sh
+++ build/flatpak/element.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-env TMPDIR="$XDG_RUNTIME_DIR/app/${FLATPAK_ID}" /app/Element/element-desktop --no-sandbox "$@"
+env TMPDIR="$XDG_RUNTIME_DIR/app/${FLATPAK_ID}" /app/element/element-desktop --no-sandbox "$@"
