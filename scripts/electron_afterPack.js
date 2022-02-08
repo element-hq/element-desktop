68ebed203fffddafa65db8a2de43381232b1971d
Michael Telatynski <7t3chguy@gmail.com>
2022-02-08 13:58:17 +0000

    Build option to strip non-proprietary ffmpeg codecs

--- scripts/electron_afterPack.js
+++ scripts/electron_afterPack.js
@@ -1,8 +1,22 @@
 const fsProm = require('fs').promises;
 const path = require('path');
+const { getElectronVersion } = "app-builder-lib/out/electron/electronVersion";
 
 exports.default = async function(context) {
-    const { electronPlatformName, appOutDir } = context;
+    const { electronPlatformName, arch, packager, appOutDir } = context;
+
+    if (process.env["ELEMENT_BUILD_FREE_FFMPEG"]) {
+        const electronVersion = await getElectronVersion(packager.projectDir);
+        await new Promise(resolve => {
+            require("electron-packager-plugin-non-proprietary-codecs-ffmpeg")(
+                appOutDir,
+                electronVersion,
+                electronPlatformName,
+                arch,
+                resolve,
+            );
+        });
+    }
 
     // Squirrel windows will try to relaunch the app using an executable of the same name as
     // before in the new version, so will fail if the executable is now called something else.
