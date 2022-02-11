05140fc0229d6ab16a076a5765c577b1184144c2
Šimon Brandner <simon.bra.ag@gmail.com>
2022-02-11 08:52:34 +0100

    Switch to IPC channel for `getDesktopCapturerSources`
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/electron-main.ts
+++ src/electron-main.ts
@@ -19,7 +19,17 @@ limitations under the License.
 
 // Squirrel on windows starts the app with various flags as hooks to tell us when we've been installed/uninstalled etc.
 import "./squirrelhooks";
-import { app, ipcMain, powerSaveBlocker, BrowserWindow, Menu, autoUpdater, protocol, dialog } from "electron";
+import {
+    app,
+    ipcMain,
+    powerSaveBlocker,
+    BrowserWindow,
+    Menu,
+    autoUpdater,
+    protocol,
+    dialog,
+    desktopCapturer,
+} from "electron";
 import AutoLaunch from "auto-launch";
 import path from "path";
 import windowStateKeeper from 'electron-window-state';
@@ -501,6 +511,13 @@ ipcMain.on('ipcCall', async function(ev, payload) {
                 // logins from the time of riot.im)
                 await keytar.deletePassword("riot.im", `${args[0]}|${args[1]}`);
             } catch (e) {}
+            break;
+        case 'getDesktopCapturerSources':
+            ret = (await desktopCapturer.getSources(args[0])).map((source) => ({
+                id: source.id,
+                name: source.name,
+                thumbnailURL: source.thumbnail.toDataURL(),
+            }));
             break;
 
         default:
