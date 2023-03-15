48f32454ce40fd1af7d68f421ff49130f2248679
Michael Telatynski <7t3chguy@gmail.com>
2023-03-15 13:40:10 +0000

    Increase timeout

--- test/launch-test.ts
+++ test/launch-test.ts
@@ -48,7 +48,7 @@ describe("App launch", () => {
             },
         });
         window = await app.firstWindow();
-    }, 30000);
+    }, 60000);
 
     afterAll(async () => {
         await app?.close().catch((e) => {
@@ -62,5 +62,5 @@ describe("App launch", () => {
         await window.locator(".mx_Welcome").waitFor();
         await expect(window).toMatchURL("vector://vector/webapp/#/welcome");
         await window.screenshot({ path: path.join(artifactsPath, "welcome.png") });
-    }, 30000);
+    }, 60000);
 });
