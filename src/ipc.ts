cfb1602f05b9fa9565d9f8a46dee4ae25750cb96
Michael Telatynski <7t3chguy@gmail.com>
2023-03-22 09:10:15 +0000

    Iterate

--- src/ipc.ts
+++ src/ipc.ts
@@ -187,50 +187,52 @@ ipcMain.on("ipcCall", async function (_ev: IpcMainEvent, payload) {
             break;
 
         case "breadcrumbs": {
-            const { TouchBarPopover, TouchBarButton } = TouchBar;
+            if (process.platform === "darwin") {
+                const { TouchBarPopover, TouchBarButton } = TouchBar;
 
-            const recentsBar = new TouchBar({
-                items: args[0].map((r: { roomId: string; avatarUrl: string | null; initial: string }) => {
-                    const defaultColors = ["#0DBD8B", "#368bd6", "#ac3ba8"];
-                    let total = 0;
-                    for (let i = 0; i < r.roomId.length; ++i) {
-                        total += r.roomId.charCodeAt(i);
-                    }
+                const recentsBar = new TouchBar({
+                    items: args[0].map((r: { roomId: string; avatarUrl: string | null; initial: string }) => {
+                        const defaultColors = ["#0DBD8B", "#368bd6", "#ac3ba8"];
+                        let total = 0;
+                        for (let i = 0; i < r.roomId.length; ++i) {
+                            total += r.roomId.charCodeAt(i);
+                        }
 
-                    const button = new TouchBarButton({
-                        label: r.initial,
-                        backgroundColor: defaultColors[total % defaultColors.length],
-                        click: (): void => {
-                            global.mainWindow?.loadURL(`vector://vector/webapp/#/room/${r.roomId}`);
-                        },
-                    });
-                    if (r.avatarUrl) {
-                        fetch(r.avatarUrl)
-                            .then((resp) => {
-                                if (!resp.ok) return;
-                                return resp.arrayBuffer();
-                            })
-                            .then((arrayBuffer) => {
-                                const buffer = Buffer.from(arrayBuffer!);
-                                button.icon = nativeImage.createFromBuffer(buffer);
-                                button.label = "";
-                                button.backgroundColor = "";
-                            });
-                    }
-                    return button;
-                }),
-            });
-
-            const touchBar = new TouchBar({
-                items: [
-                    new TouchBarPopover({
-                        label: "Recents",
-                        showCloseButton: true,
-                        items: recentsBar,
+                        const button = new TouchBarButton({
+                            label: r.initial,
+                            backgroundColor: defaultColors[total % defaultColors.length],
+                            click: (): void => {
+                                global.mainWindow?.loadURL(`vector://vector/webapp/#/room/${r.roomId}`);
+                            },
+                        });
+                        if (r.avatarUrl) {
+                            fetch(r.avatarUrl)
+                                .then((resp) => {
+                                    if (!resp.ok) return;
+                                    return resp.arrayBuffer();
+                                })
+                                .then((arrayBuffer) => {
+                                    const buffer = Buffer.from(arrayBuffer!);
+                                    button.icon = nativeImage.createFromBuffer(buffer);
+                                    button.label = "";
+                                    button.backgroundColor = "";
+                                });
+                        }
+                        return button;
                     }),
-                ],
-            });
-            global.mainWindow.setTouchBar(touchBar);
+                });
+
+                const touchBar = new TouchBar({
+                    items: [
+                        new TouchBarPopover({
+                            label: "Recents",
+                            showCloseButton: true,
+                            items: recentsBar,
+                        }),
+                    ],
+                });
+                global.mainWindow.setTouchBar(touchBar);
+            }
             break;
         }
 
