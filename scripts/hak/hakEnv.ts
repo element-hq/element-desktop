a0f92941fc033528eca3a35ab6f23a2b6d4a8eec
Li-Wen Hsu <lwhsu.github@lwhsu.org>
2023-08-09 17:11:27 +0800

    Add FreeBSD support (#1163)
    Co-authored-by: Hiroki Tagato <tagattie@gmail.com>

--- scripts/hak/hakEnv.ts
+++ scripts/hak/hakEnv.ts
@@ -81,6 +81,10 @@ export default class HakEnv {
         return this.target.platform === "linux";
     }
 
+    public isFreeBSD(): boolean {
+        return this.target.platform === "freebsd";
+    }
+
     public getTargetArch(): Arch {
         return this.target.arch;
     }
@@ -102,6 +106,6 @@ export default class HakEnv {
     }
 
     public wantsStaticSqlCipher(): boolean {
-        return !this.isLinux() || process.env.SQLCIPHER_BUNDLED == "1";
+        return !(this.isLinux() || this.isFreeBSD()) || process.env.SQLCIPHER_BUNDLED == "1";
     }
 }
