42e4d6362e76630e1bbe36c24acd6dcbc6b0d47b
Michael Telatynski <7t3chguy@gmail.com>
2023-08-15 15:06:05 +0100

    Remove pipe pluralisation separator in i18n files

--- src/language-helper.ts
+++ src/language-helper.ts
@@ -74,7 +74,6 @@ export class AppLocalization {
     public constructor({ store, components = [] }: { store: TypedStore; components: Component[] }) {
         counterpart.registerTranslations(FALLBACK_LOCALE, this.fetchTranslationJson("en_EN"));
         counterpart.setFallbackLocale(FALLBACK_LOCALE);
-        counterpart.setSeparator("|");
 
         if (Array.isArray(components)) {
             this.localizedComponents = new Set(components);
