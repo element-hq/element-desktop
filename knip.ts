c613ac0d47cee0c32ed0dfcf176134be5b0bc729
Michael Telatynski <7t3chguy@gmail.com>
2024-01-17 10:49:08 +0000

    Add knip config
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

---
+++ knip.ts
@@ -0,0 +1,14 @@
+export default {
+    entry: ["src/electron-main.ts", "src/preload.ts", "electron-builder.js", ".eslintrc-*.js", "scripts/**", "hak/**"],
+    project: ["**/*.{js,ts}"],
+    ignoreDependencies: [
+        // Brought in via hak scripts
+        "keytar",
+        "matrix-seshat",
+        // Needed by `electron-builder`
+        "electron-builder-squirrel-windows",
+        // Required for `action-validator`
+        "@action-validator/*",
+    ],
+    ignoreBinaries: ["jq", "scripts/in-docker.sh"],
+};
