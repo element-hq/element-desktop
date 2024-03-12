0c5464c38a314ad7ccf7a70db007c6ea2e3e4eae
Michael Telatynski <7t3chguy@gmail.com>
2024-03-12 17:29:03 +0000

    Fix electron-main.js path
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- playwright/element-desktop-test.ts
+++ playwright/element-desktop-test.ts
@@ -33,7 +33,7 @@ export const test = base.extend<{ app: ElectronApplication; tmpDir: string }>({
         const executablePath = process.env["ELEMENT_DESKTOP_EXECUTABLE"];
         if (!executablePath) {
             // Unpackaged mode testing
-            args.unshift("./lib/electron-main.js");
+            args.unshift(path.join(__dirname, "..", "lib", "electron-main.js"));
         }
 
         const app = await electron.launch({
