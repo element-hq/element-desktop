1b6a9be673f3dc11e8e43ee3b6c3c44ca10cd357
Michael Telatynski <7t3chguy@gmail.com>
2025-02-28 09:21:32 +0000

    Specify node version to avoid NAPI keytar bug
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- dockerbuild/setup.sh
+++ dockerbuild/setup.sh
@@ -3,5 +3,6 @@
 set -x
 declare -A archMap=(["amd64"]="x64" ["arm64"]="arm64")
 ARCH="${archMap["$TARGETARCH"]}"
+NODE_VERSION=$(cat .node-version)
 curl --proto "=https" -L "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-$TARGETOS-$ARCH.tar.gz" | tar xz -C /usr/local --strip-components=1 && \
   unlink /usr/local/CHANGELOG.md && unlink /usr/local/LICENSE && unlink /usr/local/README.md
