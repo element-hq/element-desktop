f161d6694d541c2c2f9ffb0797958bb04c74ef3c
Michael Telatynski <7t3chguy@gmail.com>
2025-10-08 14:49:43 +0100

    Tidy knip config
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- knip.ts
+++ knip.ts
@@ -1,11 +1,9 @@
 import { KnipConfig } from "knip";
 
 export default {
-    entry: ["src/electron-main.ts", "src/preload.ts", "electron-builder.ts", ".eslintrc-*.js", "scripts/**", "hak/**"],
+    entry: ["src/preload.cts", "electron-builder.ts", "scripts/**", "hak/**"],
     project: ["**/*.{js,ts}"],
     ignoreDependencies: [
-        // Brought in via hak scripts
-        "matrix-seshat",
         // Required for `action-validator`
         "@action-validator/*",
         // Used for git pre-commit hooks
