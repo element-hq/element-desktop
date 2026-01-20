781d29c51c17f9d980777d46fed4daa2bd838bb4
Michael Telatynski <7t3chguy@gmail.com>
2026-01-20 15:16:44 +0000

    Generate badge icon for macOS DMG
    to differentiate mounted DMG from executable
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- electron-builder.ts
+++ electron-builder.ts
@@ -162,6 +162,10 @@ const config: Omit<Writable<Configuration>, "electronFuses"> & {
         mergeASARs: true,
         x64ArchFiles: "**/matrix-seshat/*.node", // hak already runs lipo
     },
+    dmg: {
+        icon: "",
+        badgeIcon: "build/icon.icon",
+    },
     win: {
         target: ["squirrel", "msi"],
         signtoolOptions: {
