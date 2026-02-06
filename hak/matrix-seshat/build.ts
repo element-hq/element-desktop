97322865912dce4dc75c0323ef5c70598f032225
Michael Telatynski <7t3chguy@gmail.com>
2026-02-06 10:46:28 +0000

    matrix-seshat still uses yarn
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- hak/matrix-seshat/build.ts
+++ hak/matrix-seshat/build.ts
@@ -16,8 +16,8 @@ export default async function (hakEnv: HakEnv, moduleInfo: DependencyInfo): Prom
         env.CARGO_BUILD_TARGET = hakEnv.getTargetId();
     }
 
-    console.log("Running pnpm install");
-    await hakEnv.spawn("pnpm", ["install"], {
+    console.log("Running yarn install");
+    await hakEnv.spawn("yarn", ["install"], {
         cwd: moduleInfo.moduleBuildDir,
         env,
         shell: true,
@@ -26,7 +26,7 @@ export default async function (hakEnv: HakEnv, moduleInfo: DependencyInfo): Prom
     const buildTarget = hakEnv.wantsStaticSqlCipher() ? "build-bundled" : "build";
 
     console.log("Running pnpm build");
-    await hakEnv.spawn("pnpm", ["run", buildTarget], {
+    await hakEnv.spawn("yarn", ["run", buildTarget], {
         cwd: moduleInfo.moduleBuildDir,
         env,
         shell: true,
